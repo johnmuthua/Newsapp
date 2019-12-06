d1c390b231db68f4599a45e3534dc097356343b9
cavein <muthuahjohn@gmail.com>
2019-12-06 11:59:10 +0300

    version one complete

--- newsappproject/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-country = 'us'
-KEY = 'fa22f16f499743189d15691ded47f60b'
-url = ('https://newsapi.org/v2/top-headlines?'
-           'country='+ country+ '&'
-           'apiKey=' + KEY)
-response = requests.get(url).json()
-print(response)
